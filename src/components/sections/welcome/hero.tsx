8d2fe78d09ca5d6f7d0583cbceb389b415b5cd34
Arthur <arthur.hauenstein@universo.univates.br>
2024-04-07 18:30:31 -0300

    Hero blur image test

--- src/components/sections/welcome/hero.tsx
+++ src/components/sections/welcome/hero.tsx
@@ -1,6 +1,7 @@
 import { Highlight } from "@/components/ui/highlight";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
+import ArthurPhoto from "../../../../public/images/me.jpeg";
 import Image from "next/image";
 
 export default function Hero() {
@@ -41,12 +42,29 @@ export default function Hero() {
         </p>
       </div>
       <Image
-        className="w-96 h-96 md:w-72 lg:w-96 md:h-auto select-none object-cover md:object-contain rounded-3xl mx-auto"
-        src="/images/me.jpeg"
+        className="w-96 h-auto md:w-72 lg:w-96 md:h-auto select-none object-cover md:object-contain rounded-3xl mx-auto"
         alt="Arthur"
-        width={1487}
-        height={1982}
+        src={ArthurPhoto}
+        loading="lazy"
+        placeholder="blur"
+        width={384}
+        height={491.35}
       />
     </div>
   );
 }
+
+// Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+const keyStr =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+const triplet = (e1: number, e2: number, e3: number) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r: number, g: number, b: number) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
